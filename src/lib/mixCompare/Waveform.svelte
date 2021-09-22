0dba66b103212386047f06ad7be23140c2882392
BegottenSon <danielbrown2d@gmail.com>
2021-09-21 20:39:38 -0400

    go

--- src/lib/mixCompare/Waveform.svelte
+++ src/lib/mixCompare/Waveform.svelte
@@ -1,6 +1,6 @@
 <script>
     import { afterUpdate } from 'svelte';
-    import { drawAudio } from "./drawaudio";
+    // import { drawAudio } from "./drawaudio";
     // afterUpdate(()=> {
     //     let wavesurfer = WaveSurfer.create({
     //     container: '#waveform',
@@ -8,7 +8,7 @@
     //     progressColor: 'purple'
     // });
     // wavesurfer.load('/under-pressure.wav');
-    drawAudio('/under-pressure.wav')
+    // drawAudio('/under-pressure.wav')
 
 </script>
 <div id="waveform"></div>
